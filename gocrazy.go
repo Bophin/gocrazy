8d853606bd550d1a59fe0844c8fb49c10e098bac
Bophin <olov.langstrom@gmail.com>
2012-10-16 20:01:24 +0200

    Made a basic webserver. Reads the html in webstuff/index.html hopefully.

--- gocrazy.go
+++ gocrazy.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"html/template"
 )
+
+var templ = template.Must(template.ParseFiles("webbstuff/index.html"))
 
 func main() {
 	fmt.Println("Hej!")
-
 	fmt.Println("Hej världen! Jag är med <3")
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	templ.Execute(w, nil)
+}
