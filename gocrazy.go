33634200a008a1d82cd6f4fd90cbf109d98f0bc0
Olov Långström <boffin@mint>
2012-11-15 20:22:43 +0100

    Added -p flag for setting listening port

--- gocrazy.go
+++ gocrazy.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"net/http"
+	"flag"
 )
 
-
+var port = flag.String("p", "80", "Sets server port")
 
 func main() {
+	flag.Parse()
+
 	http.HandleFunc("/", rootHandler)
 	//Make sure nothing important lies in "main_web"
 	http.Handle("/main_web/", http.StripPrefix("/main_web/", http.FileServer(http.Dir("main_web"))))
 	http.HandleFunc("/login/", loginHandler);
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":" + *port, nil)
 }
 
